a079aea271eefda09190ae7c3baafb430931f16c
tiaghoul <tiaguinhomartins_@hotmail.com>
2016-10-25 14:50:56 +0100

    Implemented distribution of matrices

--- main.cpp
+++ main.cpp
@@ -14,16 +14,18 @@
 
 int **theMatrix;
 int *theMatrixData;
-int **smallMatrix1;
-int **smallMatrix2;
-int **smallMatrix3;
-int **smallMatrix4;
+int **localMatrix;
+int **smallMatrixA;
+int **smallMatrixB;
+int **smallMatrixC;
 
 int myRank, cartRank;
 int rowMatrix, columnMatrix;
 int Q, nProc, nNodes;
 int myRow, myCol;
 MPI_Comm cartComm, rowComm, colComm;
+MPI_Status status;
+
 
 int checkIfPossible( int p, int n){
     printf("Está a verificar se é possivel....\n");
@@ -57,35 +59,35 @@ void prepareMatrixes(){
     int dims[2];
     int period[2] = {1,1};
     int cartCoords[2];
+    int dimsSub[2] = {};
 
     MPI_Bcast(&nNodes, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
     int* auxMatrix = new int[Q*Q];
-    smallMatrix1 = new int*[Q];
+    localMatrix = new int*[Q];
     for(i=0; i<Q; i++){
-        smallMatrix1[i] = &auxMatrix[i*Q];
+        localMatrix[i] = &auxMatrix[i*Q];
     }
 
     auxMatrix = new int[Q*Q];
-    smallMatrix2 = new int*[Q];
+    smallMatrixA = new int*[Q];
     for(i=0; i<Q;i++){
-        smallMatrix2[i] = &auxMatrix[i*Q];
+        smallMatrixA[i] = &auxMatrix[i*Q];
     }
 
     auxMatrix = new int[Q*Q];
-    smallMatrix3 = new int*[Q];
+    smallMatrixB = new int*[Q];
     for(i=0; i<Q; i++){
-        smallMatrix3[i] = &auxMatrix[i*Q];
+        smallMatrixB[i] = &auxMatrix[i*Q];
     }
 
     auxMatrix = new int[Q*Q];
-    smallMatrix4 = new int*[Q];
+    smallMatrixC = new int*[Q];
     for(i=0; i<Q; i++){
-        smallMatrix4[i] = &auxMatrix[i*Q];
+        smallMatrixC[i] = &auxMatrix[i*Q];
     }
 
-
-    MPI_Dims_create(nProc, 2, dims);
+    MPI_Dims_create(nProc, 2, dims);;
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, 1, &cartComm);
     MPI_Comm_rank(cartComm, &cartRank);
 
@@ -93,9 +95,50 @@ void prepareMatrixes(){
     myRow = cartCoords[0];
     myCol = cartCoords[1];
 
+    dimsSub[0] = 0;
+    dimsSub[1] = 1;
+    MPI_Cart_sub(cartComm, dimsSub, &rowComm);
 
+    dimsSub[0] = 1;
+    dimsSub[1] = 0;
+    MPI_Cart_sub(cartComm, dimsSub, &colComm);
 
     printf("Saiu do prepareMatrixes\n");
+}
+
+void writeToMatrixes(){
+    int rankToSend;
+    MPI_Datatype qqMatrix;
+    MPI_Type_vector(Q,Q,nNodes,MPI_INT, &qqMatrix);
+    MPI_Type_commit(&qqMatrix);
+
+    if(myRank == ROOT){
+        for(int i =0; i<=Q; i+=Q){
+            for(int j=0; j<=Q; j+=Q){
+                int coordsToRank[2] = {i/Q, j/Q};
+
+                MPI_Cart_rank(cartComm, coordsToRank, &rankToSend);
+
+
+                if(rankToSend == ROOT){
+                    for(int ii = i; ii< i+Q; ii++){
+                        for(int jj = j; jj<j+Q; jj++){
+                            localMatrix[i][j] = theMatrix[i][j];
+                        }
+                    }
+                }
+                else {
+                    MPI_Send(&theMatrix[i][j], 1, qqMatrix, rankToSend, 1, cartComm);
+                }
+
+            }
+        }
+    }
+
+    else{
+        MPI_Recv(localMatrix, 1, qqMatrix, ROOT, 1, cartComm, &status);
+    }
+
 }
 
 void printMatrix(){
@@ -107,7 +150,6 @@ void printMatrix(){
 }
 
 int main(int argc, char *argv[]) {
-    MPI_Status status;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nProc);
@@ -124,6 +166,8 @@ int main(int argc, char *argv[]) {
             return 1;
         }
 
+        MPI_Bcast(&Q, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+
         theMatrix = new int*[nNodes];
         theMatrixData = new int[nNodes*nNodes];
 
@@ -137,10 +181,10 @@ int main(int argc, char *argv[]) {
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
-    double startTiem = MPI_Wtime();
+    double startTime = MPI_Wtime();
 
     prepareMatrixes();
-
+    writeToMatrixes();
 
     double finish = MPI_Wtime();
 
